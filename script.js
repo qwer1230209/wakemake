3265a7d408c6ecad37d0cd59e169b2cf43a08869
qwer1230209 <qwer1230209@naver.com>
2019-01-17 16:39:34 +0900

    4commit

--- script.js
+++ script.js
@@ -42,3 +42,24 @@ $(function(){
       });
 
 
+ $(document).ready(function() {
+            var $item = $('div.item'), //Cache your DOM selector
+            visible = 4, //Set the number of items that will be visible
+            index = 0, //Starting index
+            endIndex = ($item.length / visible) - 1; //End index
+
+            $('div#arrowR').click(function() {
+                debugger;
+                if (index < endIndex) {
+                    index++;
+                    $item.animate({ 'left': '-=600px' });//Set width of your div here
+                }
+            });
+
+            $('div#arrowL').click(function() {
+                if (index > 0) {
+                    index--;
+                    $item.animate({ 'left': '+=600px' });//Set width of your div here
+                }
+            });
+        });
